f77f886cd1c7a9a1bd671acca787ae3c53f2ca26
AreebMianoor <areebilyasmianoor@gmail.com>
2024-09-21 14:21:48 -0400

    table api added

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from .models import Menu
-from .serializers import MenuSerializer 
-
+from .models import Menu, Booking
+from .serializers import MenuSerializer, UserSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 def say_Hello(request):
     return HttpResponse("Hello from Little Lemon!")
@@ -20,3 +21,8 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
