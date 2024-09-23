ea8947386eea6bccdfba076538780548e218b554
AreebMianoor <areebilyasmianoor@gmail.com>
2024-09-22 20:19:33 -0400

    authentication api phase 1

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r'tables', views.BookingViewSet)
+router.register(r'tables', views.UserViewSet)
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('menu/', views.MenuItemsView.as_view(), name="menu-items"),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name="menu-item"),
     path('booking/', include(router.urls)), 
-
+    path('message/', views.msg, name='protected-message'), 
+    path('api-token-auth/', obtain_auth_token),
 
 ]   
+
--- restaurant/views.py
+++ restaurant/views.py
@@ -5,6 +5,9 @@ from .models import Menu, Booking
 from .serializers import MenuSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response 
+
 # Create your views here.
 def say_Hello(request):
     return HttpResponse("Hello from Little Lemon!")
@@ -16,6 +19,7 @@ class MenuItemsView(generics.ListCreateAPIView):
 
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
 
@@ -26,3 +30,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
